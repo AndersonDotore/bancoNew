5cc6dc8aa2ed9230db8e7055246b60c5899524a3
Anderson Dotore <anderson.dotore@outlook.com>
2023-09-01 16:10:15 -0300

    V1 - Variaveis

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bancoNew
+
 Criação de uma banco como forma de aprendizado e aperfeiçoamento na linguagem Python
+
+Versão 1:
+Somente com variaveis. 
---
+++ conta.py
@@ -0,0 +1,63 @@
+
+print("========= Bem vindo ao Banco New ===========")
+print("====== Selecione uma opção do menu abaixo =======")
+
+menu ="""
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+    
+    if opcao == "d":
+        valor = float(input("informe o valor do depósito:R$ "))
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+        else:
+            print("Operação com erro, valor informado é inválido")
+        
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque:R$ "))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+        if excedeu_saldo:
+            print("Operação falhou! Você nao tem saldo suficiente")
+            
+        elif excedeu_limite:
+            print("Operação falhou! O valor do saque escede o limite")
+        
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido")
+        
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("Operação falhou! O valor informado é inválido")                          
+        
+    elif opcao == "e":
+        print("\n =*=*=*= EXTRATO *=*=*=*=*=*=*")
+        print("Não foi realizadas movimentações" if not extrato else extrato)
+        print(f"\n Saldo: R${saldo:.2f}")
+        print("_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=")               
+                
+    elif opcao == "q":
+        break
+    
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada")
